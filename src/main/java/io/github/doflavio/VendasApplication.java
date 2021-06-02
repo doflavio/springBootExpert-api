232115e0502743b849095a52897da57b4efb7a84
doflavio <doflavio@gmail.com>
2021-06-01 23:09:56 -0300

    Limpeza de código

--- src/main/java/io/github/doflavio/VendasApplication.java
+++ src/main/java/io/github/doflavio/VendasApplication.java
@@ -1,11 +1,25 @@
 package io.github.doflavio;
 
+import io.github.doflavio.domain.entity.Cliente;
+import io.github.doflavio.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Fulano");
+            clientes.save(c);
+        };
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class,args);
--- src/main/java/io/github/doflavio/rest/controller/ClienteController.java
+++ src/main/java/io/github/doflavio/rest/controller/ClienteController.java
@@ -1,2 +1,64 @@
-package io.github.doflavio.rest.controller;public class ClienteController {
+package io.github.doflavio.rest.controller;
+
+import io.github.doflavio.domain.entity.Cliente;
+import io.github.doflavio.domain.repository.Clientes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.ws.Response;
+import java.util.Arrays;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            /*
+            HttpHeaders headers = new HttpHeaders();
+            headers.put("Autorization", Arrays.asList("token"));
+
+            ResponseEntity<Cliente> responseEntity =
+                    new ResponseEntity<>(cliente.get(), headers, HttpStatus.OK);
+
+            return responseEntity;
+            */
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+
+    }
+
+
+
+    @RequestMapping(value="/hello/{nome}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloCliente(@PathVariable("nome") String nomeCliente) {
+        return String.format("Hello %s",nomeCliente);
+    }
+/*
+    @RequestMapping(
+            value= {"/hello/{nome}","/api/hello/outraforma/{nome}" },
+            method = RequestMethod.GET,
+            consumes = {"application/json","application/xml"},
+            produces = {"application/json","application/xml"}
+
+    )
+    */
+
+
 }
