4c89ca4e6e60e6fcde69523ff4eb89f162b2f5a9
doflavio <doflavio@gmail.com>
2021-06-04 16:00:50 -0300

    Implementando o método de autenticacao de usuarios

--- src/main/java/io/github/doflavio/rest/controller/UsuarioController.java
+++ src/main/java/io/github/doflavio/rest/controller/UsuarioController.java
@@ -1,11 +1,18 @@
 package io.github.doflavio.rest.controller;
 
 import io.github.doflavio.domain.entity.Usuario;
+import io.github.doflavio.exception.SenhaInvalidaException;
+import io.github.doflavio.rest.dto.CredenciaisDTO;
+import io.github.doflavio.rest.dto.TokenDTO;
+import io.github.doflavio.security.jwt.JwtService;
 import io.github.doflavio.service.impl.UsuarioServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -15,8 +22,8 @@ import javax.validation.Valid;
 public class UsuarioController {
 
     private final UsuarioServiceImpl usuarioService;
-
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -24,5 +31,22 @@ public class UsuarioController {
         String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
         usuario.setSenha(senhaCriptografada);
         return usuarioService.salvar(usuario);
+    }
+
+    @PostMapping("/auth")
+    public TokenDTO autenticar(@RequestBody CredenciaisDTO credenciais){
+        try {
+            Usuario usuario = Usuario.builder()
+                    .login(credenciais.getLogin())
+                    .senha(credenciais.getSenha())
+                    .build();
+
+            UserDetails usuarioAutenticado = usuarioService.autenticar(usuario);
+            String token = jwtService.gerarToken(usuario);
+            return new TokenDTO(usuario.getLogin(), token);
+
+        }catch (UsernameNotFoundException | SenhaInvalidaException e){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
     }
 }
--- src/main/java/io/github/doflavio/service/impl/UsuarioServiceImpl.java
+++ src/main/java/io/github/doflavio/service/impl/UsuarioServiceImpl.java
@@ -2,6 +2,7 @@ package io.github.doflavio.service.impl;
 
 import io.github.doflavio.domain.entity.Usuario;
 import io.github.doflavio.domain.repository.Usuarios;
+import io.github.doflavio.exception.SenhaInvalidaException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -39,5 +40,14 @@ public class UsuarioServiceImpl implements UserDetailsService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    public UserDetails autenticar(Usuario usuario){
+        UserDetails user = loadUserByUsername(usuario.getLogin());
+        boolean senhasBatem = encoder.matches(usuario.getSenha(),user.getPassword());
+        if(senhasBatem){
+            return user;
+        }
+        throw new SenhaInvalidaException();
     }
 }
