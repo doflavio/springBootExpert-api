1f3d4de531c3e68a01a76d8780c5b641f0bff01d
doflavio <doflavio@gmail.com>
2021-06-04 18:05:35 -0300

    Configurando o Swagger

---
+++ src/main/java/io/github/doflavio/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package io.github.doflavio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.github.doflavio.rest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("Api do proeto de vendas do curso Spring boot Expert - Udemy")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact("Flavio L. Pereira", "http://github.com/doflavio", "doflavio@gmail.com");
+    }
+}
