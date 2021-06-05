b98facce3f5c8fc67240ba8923b89da5e90a3970
doflavio <doflavio@gmail.com>
2021-06-04 21:42:55 -0300

    Gerando um Arquivo WAR

--- src/main/java/io/github/doflavio/VendasApplication.java
+++ src/main/java/io/github/doflavio/VendasApplication.java
@@ -6,25 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         System.out.println("Método main");
         SpringApplication.run(VendasApplication.class,args);
     }
 }
-/*
-    @Bean
-    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
-        return args -> {
-            System.out.println("CommandLineRunner");
-            Cliente c = new Cliente(null, "Fulano");
-            clientes.save(c);
-        };
-    }
-    */
 
